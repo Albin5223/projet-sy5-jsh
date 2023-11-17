c6f5336235eb2d8f0cda5b3cda080aa050705178
JIRAUD loic <loic.jiraud@etu.u-paris.fr>
2023-11-17 19:23:07 +0100

    ?

--- Makefile
+++ Makefile
@@ -4,5 +4,9 @@ EXEC = jsh
 prog:
 	gcc -Wall -c *c $(SEP) gcc -o $(EXEC) *.o -lreadline -lhistory
 
+progMac:
+
+	gcc -Wall -c *c $(SEP) gcc -o $(EXEC) *.o -L/usr/local/opt/readline/lib -lreadline -lhistory
+
 clean:
 	rm *.o $(SEP) rm $(EXEC)
--- jsh.c
+++ jsh.c
@@ -27,15 +27,18 @@ char** get_tab_of_commande  (char *commande){
 
 int execute_commande_externe(char **commande_args){
     pid_t pid = fork();
+    int recup;
+
     if(pid == 0){
         int n = execvp(commande_args[0],commande_args);
         return n;
     }
     else{
-        wait(NULL);
+        waitpid(pid, &recup, 0);
     }
-    return 0;
+    return recup;
 }
+
 int main(int argc, char const *argv[]){
     
     char *input;
@@ -53,8 +56,24 @@ int main(int argc, char const *argv[]){
         }
         if(strcmp(commande_args[0],"exit") == 0){
             break;
-        }
-        else{
+        }else if (strcmp(commande_args[0],"?") == 0){
+            HIST_ENTRY *last_entry;
+            int len = history_length;
+
+            if (len > 0) {
+                last_entry = history_get(len-1); 
+                if (last_entry != NULL){
+                    char * last_commande = malloc(sizeof(char)*strlen(last_entry->line));
+                    strcpy(last_commande,last_entry->line);
+                    printf ("%d", execute_commande_externe(get_tab_of_commande(last_commande)));
+                    free(last_commande);
+                }else{
+                    fprintf(stderr, "Pas de commande précédente\n");
+                } 
+            } else {
+                fprintf(stderr, "Historique vide\n");
+            }
+        }else{
             execute_commande_externe(commande_args);
         }
         
