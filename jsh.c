c873e1b4dcd2bd1464c3b0bac58a9af84ec72b60
YAZICI Servan <sservxf@gmail.com>
2023-11-17 18:00:39 +0100

    Ajout du pwd + chemin_du_shell (pas fini)

--- jsh.c
+++ jsh.c
@@ -5,6 +5,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#define MAX_PATH_SIZE 150
+
 char** get_tab_of_commande  (char *commande){
     int size = 0;
     for(int i = 0; i<strlen(commande);i++){
@@ -36,13 +38,46 @@ int execute_commande_externe(char **commande_args){
     }
     return 0;
 }
+
+char *execute_pwd(int max_size){
+    char *pwd = malloc(sizeof(char)*max_size);
+    if(getcwd(pwd,max_size) == NULL){
+        printf("Error in 'path_shell' : couldn't execute 'getcwd'...\n");
+        exit(1);
+    }
+    return pwd;
+}
+
+char *path_shell(int max_size, char signe){
+    char *pwd = execute_pwd(max_size);
+
+    int real_size = strlen(pwd)+1;
+    if(real_size + 2 < max_size){
+        pwd[real_size-1] = signe;
+        pwd[real_size] = ' ';
+        pwd[real_size+1] = '\0';
+        if(realloc(pwd,sizeof(char)*(real_size+2)) == NULL){
+            printf("Error in 'path_shell' : couldn't realloc...\n");
+            exit(1);
+        }
+        return pwd;
+    }
+    else{
+        printf("Error in 'path_shell' : name too long...\n");
+        exit(1);
+    }
+}
+
 int main(int argc, char const *argv[]){
     
     char *input;
     using_history();
 
     while(1){
-        input = readline("MonPrompt> ");
+        char *path = path_shell(MAX_PATH_SIZE,'$');
+        input = readline(path);
+        free(path);
+
         add_history(input);
         char **commande_args = get_tab_of_commande(input);
 
