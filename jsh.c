1195420825bedf5e834af7abe3bb08be9f2e3e64
YAZICI Servan <sservxf@gmail.com>
2023-11-17 20:23:00 +0100

    Added a function to color a string

--- jsh.c
+++ jsh.c
@@ -4,8 +4,45 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
 
 #define MAX_PATH_SIZE 150
+
+enum color {red,green,blue,yellow,cyan,white};
+
+void color_switch(char **string,enum color c){
+
+    char *new_string = calloc((strlen(*string)+1),sizeof(char));
+
+    char *color = malloc(sizeof(char)*9);
+    switch(c){
+        case red:
+            color = "\033[31m";
+            break;
+        case green:
+            color = "\033[32m";
+            break;
+        case blue:
+            color = "\033[34m";
+            break;
+        case yellow:
+            color = "\033[33m";
+            break;
+        case cyan:
+            color = "\033[36m";
+            break;
+        default:    // white
+            color = "\033[37m";
+            break;
+    }
+
+    strcat(new_string,color);
+    strcat(new_string,*string);
+    strcat(new_string,"\033[37m");
+
+    free(*string);
+    *string = new_string;
+}
 
 char** get_tab_of_commande  (char *commande){
     int size = 0;
@@ -39,33 +76,23 @@ int execute_commande_externe(char **commande_args){
     return 0;
 }
 
-char *execute_pwd(int max_size){
-    char *pwd = malloc(sizeof(char)*max_size);
-    if(getcwd(pwd,max_size) == NULL){
+char *execute_pwd(){
+    char *pwd = malloc(sizeof(char)*MAX_PATH_SIZE);
+    if(getcwd(pwd,MAX_PATH_SIZE) == NULL){
         printf("Error in 'path_shell' : couldn't execute 'getcwd'...\n");
+        exit(1);
+    }
+    if ((pwd = realloc(pwd, sizeof(char) * (strlen(pwd) + 1))) == NULL) {
+        printf("Error in 'path_shell' : couldn't realloc...\n");
         exit(1);
     }
     return pwd;
 }
 
-char *path_shell(int max_size, char signe){
-    char *pwd = execute_pwd(max_size);
-
-    int real_size = strlen(pwd)+1;
-    if(real_size + 2 < max_size){
-        pwd[real_size-1] = signe;
-        pwd[real_size] = ' ';
-        pwd[real_size+1] = '\0';
-        if(realloc(pwd,sizeof(char)*(real_size+2)) == NULL){
-            printf("Error in 'path_shell' : couldn't realloc...\n");
-            exit(1);
-        }
-        return pwd;
-    }
-    else{
-        printf("Error in 'path_shell' : name too long...\n");
-        exit(1);
-    }
+char *path_shell(char *signe){
+    char *pwd = execute_pwd();
+    strcat(pwd,signe);
+    return pwd;
 }
 
 int main(int argc, char const *argv[]){
@@ -74,7 +101,8 @@ int main(int argc, char const *argv[]){
     using_history();
 
     while(1){
-        char *path = path_shell(MAX_PATH_SIZE,'$');
+        char *path = path_shell("$ ");
+        color_switch(&path,green);
         input = readline(path);
         free(path);
 
@@ -85,6 +113,9 @@ int main(int argc, char const *argv[]){
         while(commande_args[i] != NULL){
             printf("%s\n",commande_args[i]);
             i++;
+        }
+        if(strcmp(commande_args[0],"cd") == 0){
+            chdir(commande_args[1]);
         }
         if(strcmp(commande_args[0],"exit") == 0){
             break;
