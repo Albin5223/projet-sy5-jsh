774208a268c9a91fa233dbeaae12e52d5e8014a0
paris <paris.albin23@gmail.com>
2023-12-02 20:28:49 +0100

    Add redirection

--- jsh.c
+++ jsh.c
@@ -5,6 +5,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <sys/wait.h>
+#include "redirection.h"
 
 /**
  * Represents the maximum size of the path
@@ -170,7 +171,7 @@ int execute_cd(char **commande_args,char **precedent){
     if(size == 1 || strcmp( commande_args[1],"$HOME")==0 ){
         char *home = getenv("HOME");
         if(home == NULL){
-            printf("Erreur: la variable d'environnement HOME n'est pas définie\n");
+            fprintf(stderr,"Erreur: la variable d'environnement HOME n'est pas définie\n");
             free(pwd);
             return 1;
         }
@@ -181,7 +182,7 @@ int execute_cd(char **commande_args,char **precedent){
     }
     if(size > 2){
         free(pwd);
-        printf("Erreur: cd prend un seul %d argument\n", size);
+        fprintf(stderr,"Erreur: cd prend un seul %d argument\n", size);
         return 1;
     }
     if(strcmp(commande_args[1],"-") == 0){
@@ -190,7 +191,7 @@ int execute_cd(char **commande_args,char **precedent){
     else{
         int n = chdir(commande_args[1]);
           if(n == -1){
-            printf("Erreur: le dossier n'existe pas\n");
+            fprintf(stderr,"Erreur: le dossier n'existe pas\n");
             free(pwd);
             return 1;
         }
@@ -216,7 +217,7 @@ char *path_shell(char *signe, enum color job, enum color path){
 
     char *prompt = calloc(sizeof(char),strlen(pwd)+strlen(jobs)+strlen(signe)+1);  // Allocating the prompt
     if (prompt == NULL) {
-        printf("Error in 'path_shell' : couldn't realloc...\n");
+        fprintf(stderr,"Error in 'path_shell' : couldn't realloc...\n");
         exit(1);
     }
 
@@ -230,10 +231,18 @@ char *path_shell(char *signe, enum color job, enum color path){
     return prompt;
 }
 
+
 int main(int argc, char const *argv[]){
     
     char *input;
     char *precedent = execute_pwd();
+
+    int descripteur_sortie_standart;
+    int descripteur_sortie_erreur;
+
+    int copie_sortie_standart = dup(STDOUT_FILENO);
+    int copie_sortie_erreur = dup(STDERR_FILENO);
+
    
     int last_return_code = 0;
     
@@ -241,6 +250,8 @@ int main(int argc, char const *argv[]){
     rl_outstream = stderr;
 
     while(1){
+        descripteur_sortie_erreur = -1;
+        descripteur_sortie_standart = -1;
         char *path = path_shell("$ ",green,blue);
         
         input = readline(path);
@@ -252,9 +263,32 @@ int main(int argc, char const *argv[]){
             continue;
         }
 
+        char **commande_args;
         add_history(input);
-        char **commande_args = get_tab_of_commande(input);
-
+        
+        commande_args = get_tab_of_commande(input);
+        /*
+        * Mise en place des descripteurs en cas de redirections
+        * On sait que la dernière redirection a la priorité, donc on a juste besoin de 2 descripteur, le premier pour la redirection pour la sortie standart
+        * et le deuxième pour la redirection dans la sortie erreur
+        * 
+        * Dans cette condition, si il y a la présence d'une ou plusieurs redirection, on va affecter les descripteurs correspondant puis excécuter la commande
+        * avec la suite du code
+        */
+        if(isRedirection(commande_args)!=-1){
+            int *fd = getDescriptorOfRedirection(commande_args);
+            if(fd[0] != -1){
+                descripteur_sortie_standart = fd[0];
+            }
+            if(fd[1] != -1){
+                descripteur_sortie_erreur = fd[1];
+            }
+            //On récupère la commande cad : comm > fic, 
+            commande_args = getCommandeOfRedirection(commande_args);
+            free(fd);
+            
+        }
+        
         if(strcmp(commande_args[0],"exit") == 0){
             if(commande_args[1] != NULL){
                 exit(atoi(commande_args[1]));
@@ -273,6 +307,20 @@ int main(int argc, char const *argv[]){
         else{
             last_return_code = execute_commande_externe(commande_args);
         }
+
+        /*
+        *Si les descripteurs sont difféérents de -1 alors c'est qu'il y a un fichier qui est ouvert
+        */
+        if(descripteur_sortie_erreur >0){
+            close(descripteur_sortie_erreur);
+            dup2(copie_sortie_erreur,2);
+        }
+        if(descripteur_sortie_standart >0){
+            close(descripteur_sortie_standart);
+            dup2(copie_sortie_standart,1);
+        }
+
+        
         
         free(commande_args);
         free(input);
