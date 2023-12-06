114b9f1f95a02f3ea9d79d252777b44c4c24d16a
paris <paris.albin23@gmail.com>
2023-12-06 23:37:47 +0100

    Update exit when jobs in background

--- jsh.c
+++ jsh.c
@@ -163,20 +163,28 @@ int main(int argc, char const *argv[]){
         }
 
         if(strcmp(commande_args[0],"exit") == 0){
-            if(commande_args[1] != NULL){
+            int numberOfJobs = getNbJobs();
+            if(numberOfJobs != 0){
+                printf("Il y a %d jobs en cours d'execution\n",numberOfJobs);
+                last_return_code = 1;
+            }
+            else{
+                if(commande_args[1] != NULL){
                 free(commande_args);
                 free(input);
                 free(precedent);
                 clear_history();
                 exit(atoi(commande_args[1]));
+                }
+                else{
+                    free(commande_args);
+                    free(input);
+                    free(precedent);
+                    clear_history();
+                    exit(last_return_code);
+                }
             }
-            else{
-                free(commande_args);
-                free(input);
-                free(precedent);
-                clear_history();
-                exit(last_return_code);
-            }
+            
         }
         else if (strcmp(commande_args[0],"?") == 0){
             printf("%d\n",last_return_code);
